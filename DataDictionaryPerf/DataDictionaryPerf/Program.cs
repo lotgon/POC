aa100bb6537d10456380170894b6cc63f4b37bef
Andrei Pazniak <andrei.pazniak@soft-fx.lv>
2016-05-18 11:46:53 +0300

    datadict peroformance

---
+++ DataDictionaryPerf/DataDictionaryPerf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDictionaryPerf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            Random r = new Random();
+            Stopwatch s = new Stopwatch();
+
+            int count = 1000 * 1000 *100;
+            s.Start();
+            for (int i = 0; i < count; i++)
+            {
+                string rs = r.Next(1000000).ToString();
+                int n = 0;
+                dict.TryGetValue(rs, out n);
+                n++;
+
+                dict[rs] = n;
+            }
+            s.Stop();
+
+            Console.WriteLine("Insert performance {0} records per sec", count / s.Elapsed.Seconds );
+            
+        }
+    }
+}
